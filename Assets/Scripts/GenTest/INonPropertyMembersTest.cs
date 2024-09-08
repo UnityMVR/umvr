76f24f28ac8d53a749e2e813d471845a07456789
Piotr Urbanski <piotr.s.urbanski@gmail.com>
2024-09-08 15:12:24 +0200

    Add test interface

---
+++ Assets/Scripts/GenTest/INonPropertyMembersTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using pindwin.umvr.Model;
+
+namespace GenTest
+{
+	public interface INonPropertyMembersTest : IModel
+	{
+		const int CONST0 = 0;
+		
+		static void StaticMethod0() { }
+		
+		int this[int index] { get; set; }
+		
+		int Method0();
+		int Method1(int param);
+		
+		void Method3<TParam>(TParam param);
+
+		event Action Event0;
+		event Action<int> Event1;
+	}
+}
